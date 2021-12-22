d18c3d8106c8871221d9cd101b3aa907ade28f70
zhuxuhao <zhuxuhao@cmict.chinamobile.com>
2021-12-22 15:20:06 +0800

    created for developing and set devServer-proxy

--- components/dashboard/craco.config.js
+++ components/dashboard/craco.config.js
@@ -12,5 +12,18 @@ module.exports = {
                 require('autoprefixer'),
             ],
         }
+    },
+    devServer: {
+        proxy: {
+            '/api': {
+                 target: 'https://' + 'gitpod.io',
+                 ws: true,
+                 headers: {
+                     host: 'gitpod.io',
+                     origin: 'https://' + 'gitpod.io',
+                     cookie: '_gitpod_io_=s%3A113ee820-feee-4595-8569-8becf5efd681.uiV6SDqM%2BsF5vIltaW96jj5tyqg2ZLencitW1ibYsgk'
+                }
+            }
+        }
     }
 }
