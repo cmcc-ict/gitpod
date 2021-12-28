af0d7859ab2a487d584413f731fd1a46d1375bea
zXHAO <1960292496@qq.com>
2021-12-28 12:39:04 +0000

    解决schema报错问题

--- components/dashboard/craco.config.js
+++ components/dashboard/craco.config.js
@@ -16,11 +16,11 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                 target: 'https://' + '3001-jade-asp-wom7fvac.ws-us25.gitpod.io',
+                 target: 'https://' + 'gitpod.io',
                  ws: true,
                  headers: {
-                     host: '3001-jade-asp-wom7fvac.ws-us25.gitpod.io',
-                     origin: 'https://' + '3001-jade-asp-wom7fvac.ws-us25.gitpod.io',
+                     host: 'gitpod.io',
+                     origin: 'https://' + 'gitpod.io',
                      cookie: '_gitpod_io_=s%3A0a317dbd-5e7c-4244-9f89-b7d08ee11b29.wcg0If6PEBe%2F7OLlbATXjULNKKMDDRS01pqUiQRIPP0'
                 }
             }
